928945600484185e542153d4768f2c58241a426c
tomjones1710 <ohazard1710@icloud.com>
2026-03-28 15:53:21 +0000

    Add audio file handling and switch recognition method to Sphinx

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,1 +1,16 @@
 import speech_recognition as sr
+from os import path
+
+#filename = ""
+#audio_file = path.join(path.dirname(path.realpath(__file__)), filename)
+#audio = sr.AudioData.from_file(audio_file)
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    audio = r.listen(source)
+
+text = r.recognize_amazon(audio)
+
+
+a = 4
