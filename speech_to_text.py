b1e7a3011cc7869a468a91127585477bbf52d72a
tomjones1710 <ohazard1710@icloud.com>
2026-03-29 11:14:17 +0100

    Refactor audio processing in speech_to_text.py to use whisper model for transcription

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,17 +1,20 @@
 import speech_recognition as sr
 from os import path
+import whisper
 
 #filename = ""
 #audio_file = path.join(path.dirname(path.realpath(__file__)), filename)
 #audio = sr.AudioData.from_file(audio_file)
 
 r = sr.Recognizer()
+model = whisper.load_model("turbo")
 
 def main(audio, timestamps):
     text = ""
     for i in range(len(timestamps) - 1):
-        audio_chunk = audio[timestamps[i][0]:timestamps[i+1[0]]]
-        text1 = r.recognize_amazon(audio_chunk)
+        audio_chunk = audio[timestamps[i][0]:timestamps[i+1][0]]
+        #text1 = r.recognize_whisper(audio_chunk)
+        text1 = model.transcribe(audio_chunk, language="english")["text"]
         text1 = timestamps[i][1] + ": " + text1
         text += text1 + "\n"
     return text
